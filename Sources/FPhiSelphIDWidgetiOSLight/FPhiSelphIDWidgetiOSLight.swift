b8c347a09a998647c906520bf802fa3cb9865318
Jorge Poveda <jpoveda@facephi.com>
2023-03-08 12:51:44 +0100

    [SM-1426] feat: Added Package.swift and created base class to set up the scheme

---
+++ Sources/FPhiSelphIDWidgetiOSLight/FPhiSelphIDWidgetiOSLight.swift
@@ -0,0 +1,11 @@
+//
+//  FPhiSelphIDWidgetiOSLight.swift
+//
+//
+//  Created by Jorge Poveda on 8/3/23.
+//
+
+import Foundation
+import FPhiSelphIDWidgetiOS
+
+private class FPhiSelphIDWidgetiOSLight {}
