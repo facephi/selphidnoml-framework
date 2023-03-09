a0da819bfc76d58a8028a21f7d9830140bfe1733
Jorge Poveda <jpoveda@facephi.com>
2023-03-09 12:10:27 +0100

    [SM-1428] feat: Removed ZipZap.xcframework and used the new FPhiZipZap SPM package instead

--- Package.swift
+++ Package.swift
@@ -9,20 +9,23 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FPhiSelphIDWidgetiOSLight",
-            targets: ["FPhiSelphIDWidgetiOSLight", "FPhiSelphIDWidgetiOS", "ZipZap"]),
+            targets: ["FPhiSelphIDWidgetiOSLight", "FPhiSelphIDWidgetiOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com:facephi-clienters/SDK-ZipZap-SPM.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FPhiSelphIDWidgetiOSLight",
-            dependencies: ["FPhiSelphIDWidgetiOS", "ZipZap"],
+            dependencies: [
+                "FPhiSelphIDWidgetiOS",
+                .product(name: "FPhiZipZap", package: "SDK-ZipZap-SPM")
+            ],
             resources: [.copy("fphi-selphid-widget-resources-selphid-1.0.zip")]),
-            .binaryTarget(name: "FPhiSelphIDWidgetiOS", path: "FPhiSelphIDWidgetiOS.xcframework"),
-            .binaryTarget(name: "ZipZap", path: "ZipZap.xcframework"),
+        .binaryTarget(name: "FPhiSelphIDWidgetiOS", path: "FPhiSelphIDWidgetiOS.xcframework")
     ]
 )
