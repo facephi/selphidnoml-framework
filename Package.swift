94e5913aa079777934379820dcf8d515ec919354
Jorge Poveda <jpoveda@facephi.com>
2023-03-09 17:31:47 +0100

    [SM-1426] feat: Added Microblink product

--- Package.swift
+++ Package.swift
@@ -9,13 +9,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FPhiSelphIDWidgetiOSLight",
-            targets: ["FPhiSelphIDWidgetiOSLight", "FPhiSelphIDWidgetiOS"]),
+            targets: ["FPhiSelphIDWidgetiOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "git@github.com:facephi-clienters/SDK-ZipZap-SPM.git", from: "1.0.0"),
-        .package(url: "git@github.com:facephi/microblink-framework.git", from: "5.20.1")
+        .package(url: "git@github.com:facephi/microblink-framework.git", branch: "feature/SM-1429_Add_SPM_Support")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,7 +24,8 @@ let package = Package(
             name: "FPhiSelphIDWidgetiOSLight",
             dependencies: [
                 "FPhiSelphIDWidgetiOS",
-                .product(name: "FPhiZipZap", package: "SDK-ZipZap-SPM")
+                .product(name: "FPhiZipZap", package: "SDK-ZipZap-SPM"),
+                .product(name: "Microblink", package: "microblink-framework")
             ],
             resources: [.copy("fphi-selphid-widget-resources-selphid-1.0.zip")]),
         .binaryTarget(name: "FPhiSelphIDWidgetiOS", path: "FPhiSelphIDWidgetiOS.xcframework")
