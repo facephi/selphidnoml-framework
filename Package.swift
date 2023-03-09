325ac31dd8ee4ebdcb914e54c2a518351f4a9c40
Jorge Poveda <jpoveda@facephi.com>
2023-03-09 15:45:46 +0100

    [SM-1426] feat: Added MB dependency

--- Package.swift
+++ Package.swift
@@ -14,7 +14,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:facephi-clienters/SDK-ZipZap-SPM.git", from: "1.0.0")
+        .package(url: "git@github.com:facephi-clienters/SDK-ZipZap-SPM.git", from: "1.0.0"),
+        .package(url: "git@github.com:facephi/microblink-framework.git", branch: "feature/SM-1429_Add_SPM_Support")
+        
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
